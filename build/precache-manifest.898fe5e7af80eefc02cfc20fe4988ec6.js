34e24636e29dfe56690df665808cd35aaf561824
Joe <josephcobarrubias@gmail.com>
2022-02-17 17:56:27 -0800

    deployment-ready

---
+++ build/precache-manifest.898fe5e7af80eefc02cfc20fe4988ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97eb23cbbfccc7efa79f",
+    "url": "/static/css/main.235f0ca1.chunk.css"
+  },
+  {
+    "revision": "97eb23cbbfccc7efa79f",
+    "url": "/static/js/main.c04e64b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a79dfd7c4aa1907bc1d9",
+    "url": "/static/js/2.edfbbcd3.chunk.js"
+  },
+  {
+    "revision": "92a2ad1a20163da8312ac3b492e7a702",
+    "url": "/index.html"
+  }
+];
