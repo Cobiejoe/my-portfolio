e08f24981035fcbd765b79574a3a25461b7dc5b5
Joe <josephcobarrubias@gmail.com>
2022-02-17 17:35:04 -0800

    inital commit

---
+++ src/components/About.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
+
+export default function About() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            strings: ["Software Developer."],
+            backDelay: 2000
+        })
+    }, []);
+
+    return (
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg: flex-grow md: w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Hi, I'm Joe!
+                        <br className="hidden lg:inline-block" /><span ref={textRef} className="text-blue-400"></span>
+                    </h1>
+                    <p className="mb-8 leading-relaxed">
+                        After completing my undergrad in Business Administration, I spent 4 years in the service industry, bartending and waiting tables. 
+                        After a conversation with a mentor about switching career industries, I decided to give programming a shot. What started as a curiosity
+                        and a passive, fun activity to help build some technical skills, ended as a full-fledged passion and borderline obession with the world of
+                        information technology. I fell in love with creating projects, solving dynamic problems, and spent countless hours reading documentation, following tutorials,
+                        and grinding out algorithms. I found myself enrolling in a bootcamp where I was able to strengthen my expertise and learn industry demanding skills and 
+                        technologies. After a year and a half of building credentials and facilitating my title as a developer, I'm ready to take my abilities
+                        into the professional world. Check out the rest of my site!
+                    </p>
+                    <div className="flex justify-center">
+                        <a
+                            href="#projects"
+                            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                            See my past work!
+                        </a>
+                    </div>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img 
+                        className="object-cover object-center h-96 w-96 rounded-full"
+                        alt="hero"
+                        src="./profile_img.jpg"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Apollo Music Share",
+    subtitle: "React, Apollo, GraphQL",
+    description:
+      "Music sharing app created with advanced React hooks, styled with MaterialUI, and managed with Apollo and GraphQL. Works with any valid Youtube or SoundCloud link",
+    image: "./music-share-gif.gif",
+    link: "https://cobiejoe-music-share.herokuapp.com/",
+  },
+  {
+    title: "Friend Ships",
+    subtitle: "Lightning Data Service, Lightning Web Components, Javascript",
+    description:
+      "Boat-leasing service application that allows users and sellers to browse boats for sale in their area",
+    image: "./friend-ships-gif.gif",
+    link: "https://github.com/Cobiejoe/Friend-Ships",
+  },
+  {
+    title: "2D RPG",
+    subtitle: "Java, Piskel",
+    description:
+      "2d RPG video game using advanced java classes and my own (very crude) pixel art. Work in progress!",
+    image: "./game_gif.gif",
+    link: "https://github.com/Cobiejoe/2D-RPG",
+  },
+  {
+    title: "Instagram Clone",
+    subtitle: "React Hooks, Apollo, Hasura, GraphQL",
+    description:
+      "Complete instagram Clone utilizing React hooks. (WORK IN PROGRESS)",
+    image: "./instagram_gif.gif",
+    link: "https://github.com/Cobiejoe/instagram-clone",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Spring Boot",
+  "Javascript",
+  "MySQL",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "React",
+  "Apollo",
+  "Firebase",
+  "Three.js",
+  "HTML/CSS"
+];
